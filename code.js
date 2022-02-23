a9fae155de72037a01a976cb0dcee7c7f99b2667
Howard Lin <hlin@statestitle.com>
2022-02-23 12:02:58 -0800

    Basic passphrase generation

--- code.js
+++ code.js
@@ -1,2 +1,12 @@
-console.log('hello world')
-$('h2').text('bananas')
+const numberOfWords = 3;
+let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+let words = _.map($('#words div'), function(div) { return $(div).text() });
+
+let sampleDigit = _.sample(digits);
+let sampleSymbol = _.sample(symbols);
+let sampleWords = _.sampleSize(words, numberOfWords);
+let entities = [sampleWords[0], sampleDigit, sampleWords[1], sampleSymbol, sampleWords[2]];
+let passphrase = entities.join("");
+
+$('#passphrase').text(passphrase);
