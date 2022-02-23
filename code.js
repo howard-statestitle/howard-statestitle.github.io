866dc22957291a9731aa3ce3d6db75629544a155
Howard Lin <hlin@statestitle.com>
2022-02-23 13:41:08 -0800

    Add other languages

--- code.js
+++ code.js
@@ -1,12 +1,33 @@
 const numberOfWords = 3;
-let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-let words = _.map($('#words div'), function(div) { return $(div).text() });
+const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
-let sampleDigit = _.sample(digits);
-let sampleSymbol = _.sample(symbols);
-let sampleWords = _.sampleSize(words, numberOfWords);
-let entities = [sampleWords[0], sampleDigit, sampleWords[1], sampleSymbol, sampleWords[2]];
-let passphrase = entities.join("");
+function generate() {
+  // select languages
+  let checked = $('#settings input:checkbox:checked');
+  let languages = _.map(checked, function(input) { return $(input).attr('id') });
+  let selectors = _.map(languages, function(language) { return `#words .${language}` });
+  let selector = selectors.join(',');
+  let words = _.map($(selector), function(div) { return $(div).text() });
 
-$('#passphrase').text(passphrase);
+  // sample entities
+  let sampleDigit = _.sample(digits);
+  let sampleSymbol = _.sample(symbols);
+  let sampleWords = _.sampleSize(words, numberOfWords);
+  let entities = [sampleWords[0], sampleDigit, sampleWords[1], sampleSymbol, sampleWords[2]];
+
+  // display passphrase
+  let passphrase = entities.join('');
+  $('#passphrase').text(passphrase);
+}
+
+$('#english').prop('checked', true);
+generate();
+
+$('body').on('click', function(event) {
+  let node = $(event.target);
+  let isBody = (node.prop('tagName') == 'BODY');
+  if (isBody) {
+    generate();
+  }
+})
